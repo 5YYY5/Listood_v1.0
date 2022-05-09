8f27d4b68b2a92f5d80d8f19ecc3771da4c3a6d9
5YYY5 <hhvvkkjb@gmail.com>
2022-05-09 17:33:18 +0300

    Постепенно

--- app/src/main/java/com/example/listood_v10/Activities/MainActivity.java
+++ app/src/main/java/com/example/listood_v10/Activities/MainActivity.java
@@ -26,7 +26,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     Fragment fragmentmn;
-    Button bt1screen, bt2screen, bt3screen, bt4screen;
+    ImageButton bt1screen, bt2screen, bt3screen, bt4screen;
 //    ArrayList<Day_Of_The_Week> days = new ArrayList<Day_Of_The_Week>();
 //    ArrayList<Reminders> reminders = new ArrayList<Reminders>();
 
@@ -37,10 +37,10 @@ public class MainActivity extends AppCompatActivity {
 //        RecyclerView recyclerView1 = findViewById(R.id.list1);
         setContentView(R.layout.activity_main);
         //Объявляю кнопки навигационной панели======================================================
-        bt1screen = (Button) findViewById(R.id.bt1);
-        bt2screen = (Button) findViewById(R.id.bt2);
-        bt3screen = (Button) findViewById(R.id.bt3);
-        bt4screen = (Button) findViewById(R.id.bt4);
+        bt1screen = (ImageButton) findViewById(R.id.bt1);
+        bt2screen = (ImageButton) findViewById(R.id.bt2);
+        bt3screen = (ImageButton) findViewById(R.id.bt3);
+        bt4screen = (ImageButton) findViewById(R.id.bt4);
         //Устанавливаю кликеры для кнопок===========================================================
         bt1screen.setOnClickListener(new View.OnClickListener() {
             @Override
